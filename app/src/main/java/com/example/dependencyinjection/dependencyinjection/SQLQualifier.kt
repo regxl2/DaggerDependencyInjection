fd959d7b45dbf7def2fb1207d11dc5abd9de57da
regxl <rathourabhishekvk4@gmail.com>
2024-05-21 14:54:45 +0530

    completed

---
+++ app/src/main/java/com/example/dependencyinjection/dependencyinjection/SQLQualifier.kt
@@ -0,0 +1,11 @@
+package com.example.dependencyinjection.dependencyinjection
+
+import javax.inject.Qualifier
+
+
+// Qualifiers  :- A qualifier is a custom annotation that will be used to identify a dependency.
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MessageQualifier
