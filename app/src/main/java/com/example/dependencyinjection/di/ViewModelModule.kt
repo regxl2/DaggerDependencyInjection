20d85829fd52c3ffdb42d1637782dc0a1f8b9094
regxl <rathourabhishekvk4@gmail.com>
2024-05-27 18:50:43 +0530

    completed

---
+++ app/src/main/java/com/example/dependencyinjection/di/ViewModelModule.kt
@@ -0,0 +1,5 @@
+package com.example.dependencyinjection.di
+
+class
+class ViewModelModule {
+}
---
+++ app/src/main/java/com/example/dependencyinjection/viewmodels/MainViewModel2.kt
@@ -0,0 +1,32 @@
+package com.example.dependencyinjection.viewmodels
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dependencyinjection.repository.ProductRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MainViewModel2 @Inject constructor(private val productRepository: ProductRepository): ViewModel() {
+
+    var productUiState: MutableState<ProductUiState> = mutableStateOf(ProductUiState.Loading)
+        private set
+
+    init {
+        getProducts()
+    }
+    fun getProducts(){
+        viewModelScope.launch {
+            delay(2000)
+            val response = productRepository.getProducts()
+            if(response.isSuccessful && response.body()!=null){
+                productUiState.value = ProductUiState.Success(response.body()!!)
+            }
+            else{
+                productUiState.value = ProductUiState.Error
+            }
+        }
+    }
+}
